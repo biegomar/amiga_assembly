e14e7762743f31eda0c7d8f0f44d9ee8fb549be6
biegomar <marc.biegota@web.de>
2025-12-29 10:23:29 +0100

    add first assignments

--- sample.s
+++ sample.s
@@ -6,30 +6,53 @@
 ; Main Program
 ;*************************************
 
-SECTION  CODE
+SECTION           CODE
 
-result:  DC.L                 $FF
+;*************************************
+; Variables
+;*************************************
+
+directVarResult:  DC.L      $0
+indirectResult:   DC.L      $0
 
 main:
-; Clear all data registers D0-D7
-         CLR.L                d0
-         CLR.L                d1
-         CLR.L                d2
-         CLR.L                d3
-         CLR.L                d4
-         CLR.L                d5
-         CLR.L                d6
-         CLR.L                d7
-       
-         ;indirekt 
-         move.l               #05,d0         ;move.l, to fill the register with a long value        
-         lea.l                result,a0      ;load effective address, to load the address of result into address register A0         ;move the value of d0 into the address pointed by a0                    
-         add.l                d0,(a0)
+                  JSR       clrDataRegisters
+                  JSR       addDirectRegister
+                  JSR       addDirectVariable
+                  JSR       addIndirect              
 
-         ;direkt
-         move.l               #$FF,result
-         add.l                d0,result
+                  rts  
 
-         rts                  
+;*************************************
+; Subroutines
+;*************************************
+clrDataRegisters:
+                  CLR.L     d0
+                  CLR.L     d1
+                  CLR.L     d2
+                  CLR.L     d3
+                  CLR.L     d4
+                  CLR.L     d5
+                  CLR.L     d6
+                  CLR.L     d7
+                  RTS 
+
+addDirectRegister:
+                  move.l    #$05,d0
+                  move.l    #$FF,d1
+                  ADD.L     d1,d0
+                  RTS
+
+addDirectVariable:
+                  move.l    #$FF,d1
+                  ADD.L     d1,directVarResult
+                  RTS                
+
+addIndirect:
+                  move.l    #$05,d0
+                  lea.l     indirectResult,a0
+                  move.l    #$FF,d1
+                  ADD.L     d1,(a0)
+                  RTS             
 
 END
